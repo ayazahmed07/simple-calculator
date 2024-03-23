ccecc2262049a7da7b7da6f8f2ff2b9675bfa975
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-03-23 14:21:27 +0500

    updated simplecalculator.ts

--- simplecalculator.js
+++ simplecalculator.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([{ message: "Enter first number", type: "number", name: "firstnumber" },
-    { message: "Enter second number", type: "number", name: "secondnumber" },
+const answer = await inquirer.prompt([{ message: "Enter your first number", type: "number", name: "firstnumber" },
+    { message: "Enter your second number", type: "number", name: "secondnumber" },
     { message: "Select one of the operators to perform operation",
         type: "list",
         name: "operator",
--- simplecalculator.ts
+++ simplecalculator.ts
@@ -2,8 +2,8 @@
 
 import inquirer from "inquirer";
 
-const answer = await inquirer.prompt([{message:"Enter first number", type: "number", name: "firstnumber" },
-{message: "Enter second number", type: "number", name: "secondnumber"},
+const answer = await inquirer.prompt([{message:"Enter your first number", type: "number", name: "firstnumber" },
+{message: "Enter your second number", type: "number", name: "secondnumber"},
 {message: "Select one of the operators to perform operation",
 type: "list",
 name: "operator",
