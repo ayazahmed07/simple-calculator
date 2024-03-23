98328584241902d6980416a1f328b6c0ddd7ca4e
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-03-23 14:17:04 +0500

    First Commit

---
+++ simplecalculator.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{ message: "Enter first number", type: "number", name: "firstnumber" },
+    { message: "Enter second number", type: "number", name: "secondnumber" },
+    { message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Substraction", "Multiply", "Divide"]
+    },
+]);
+// conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiply") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Divide") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("Please select valid operator");
+}
